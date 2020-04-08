42e9d5b696ca2e37225c43a19f6954b164002477
Nick Gregory <nicholas.gregory@gmail.com>
2020-04-08 07:17:03 -0700

    update and fixing ignores

--- open_file.py
+++ open_file.py
@@ -4,6 +4,12 @@
 # This assumes the folder containing your data is in the same starting directory
 # as the script you are using.
 #
+# If you need to go up a level in the tree, add ../ to the file path for each
+# level that you need to go up.
+#
+# If you want to specify the entire filepath, use r"C:\file\path\filename.csv"
+# The r in front of the filepath is intention.  This is for windows.
+#
 # ****************************************************************************
 
 
